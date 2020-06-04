6f921bbc46da258bf2006b63cc0ddd09a523876d
AbzalovaLiliya <42464223+AbzalovaLiliya@users.noreply.github.com>
2020-06-04 22:39:18 +0300

    module2-task1

--- js/stat.js
+++ js/stat.js
@@ -5,17 +5,20 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP_RECT = 10;
-var GAP_TEXT = 30;
-
+var GAP = 30;
+var INTERVAL = 100;
 var TEXT_HEIGHT = 20;
 var BAR_HEIGHT_MAX = 140;
+var BAR_WIDTH = 40;
 var MY_RESULT_COLOR = 'rgba(255, 0, 0, 1)';
-var renderCloud = function(ctx, x, y, color) {
+var BOTTOM_LINE = 250;
+
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 1; i < arr.length; i++) {
@@ -38,22 +41,22 @@ window.renderStatistics = function (ctx, players, times) {
   ctx.fillStyle = '#000000';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP_TEXT, GAP_TEXT);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP_TEXT, GAP_TEXT + TEXT_HEIGHT);
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP, GAP + TEXT_HEIGHT);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000000';
-    ctx.fillText(players[i], CLOUD_X + 100*i + 30, 250);
+    ctx.fillText(players[i], CLOUD_X + INTERVAL * i + GAP, BOTTOM_LINE);
 
     if (players[i] === 'Вы') {
       ctx.fillStyle = MY_RESULT_COLOR;
     } else {
       ctx.fillStyle = getRandomColor();
     }
-     ctx.fillRect(CLOUD_X + 100*i + 30, 240, 40, -(BAR_HEIGHT_MAX * times[i]) / maxTime);
+    ctx.fillRect(CLOUD_X + INTERVAL * i + GAP, BOTTOM_LINE - GAP_RECT, BAR_WIDTH, -(BAR_HEIGHT_MAX * times[i]) / maxTime);
     ctx.fillStyle = '#000000';
-    ctx.fillText(Math.round(times[i]), CLOUD_X + 100*i + 30, 220 - (BAR_HEIGHT_MAX * times[i]) / maxTime);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + INTERVAL * i + GAP, BOTTOM_LINE - GAP_RECT * 3 - (BAR_HEIGHT_MAX * times[i]) / maxTime);
   }
 };
